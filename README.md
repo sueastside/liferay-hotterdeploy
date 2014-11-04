e46f20b1269e14021a710719e1e44e663372eaf2
sueastside <sueastside@gmail.com>
2014-11-04 12:40:07 +0100

    Added support for java code reload using Spring Loaded

--- README.md
+++ README.md
@@ -2,43 +2,49 @@ Liferay Hotterdeploy
 ====
 HotterDeployer is a Liferay utility meant to ease development.
 
-It will monitor the resources (jsp,css,js) in your workspace for deployed
-portlets and hot-copy them on changes, without the need for a redeployment. 
-(Keeping the permgen problem at bay)
+It will monitor resources (.jsp,.js,.css,.tag,.vm,.jspf), jsp-hooks and
+java code of deployed portlets in your workspace and hot-copy them on changes,
+without the need for a redeployment. (Keeping the permgen problem at bay)
 
-It also offers an alternative to the liferay 'deploy' directory, 
+It also offers an alternative to the liferay 'deploy' directory,
 called 'hotterdeploy' which can detect conflicting jar changes and will
 automatically undeploy and redeploy the affected portlet.
 (Change the `liferay.auto.deploy.dir` property in your settings.xml accordingly
  e.g. `<liferay.auto.deploy.dir>
         /liferay-developer-studio/liferay-portal-6.2-ee-sp3/hotterdeploy
       </liferay.auto.deploy.dir>`)
-      
-If you have a [LiveReload](http://feedback.livereload.com/knowledgebase/articles/86242-how-do-i-install-and-use-the-browser-extensions-) 
-extension installed, your browser will automatically reload the page 
-when it detects a changed resource or redeployed portlet.
 
-      
+If you have a [LiveReload](http://feedback.livereload.com/knowledgebase/articles/86242-how-do-i-install-and-use-the-browser-extensions-)
+extension installed, your browser will automatically reload the page
+when it detects a change.
+
+
 Notes
------ 
+-----
 As this uses inotify to detect changed files, it is linux only at the moment.
 
 Installation
------ 
+-----
 Checkout Hotterdeploy
  ```
  git clone https://github.com/sueastside/liferay-hotterdeploy.git
  ```
- 
+
 Install Hotterdeploy
  ```
  cd liferay-hotterdeploy
  sudo python setup.py install
  ```
 
- 
+Download and configure Spring Loaded
+Get https://github.com/spring-projects/spring-loaded
+edit setenv.sh and on a new line add
+ ```
+ JAVA_OPTS="$JAVA_OPTS -javaagent:<pathTo>/springloaded-<version>.jar -noverify"
+ ```
+
 Usage
------ 
+-----
  ```
  hotterdeploy "/home/user/workspace" "/home/user/liferay-developer-studio/liferay-portal-6.2-ee-sp3/tomcat-7.0.42"
  ```
--- hotterdeploy/app.py
+++ hotterdeploy/app.py
@@ -8,7 +8,7 @@ HotterDeployer is a Liferay utility meant to ease development.
 It will monitor the resources (jsp,css,js) in your workspace for deployed
 portlets and hot-copy them on changes, without the need for a deployment.
 
-It also offers an alternative to the liferay 'deploy' directory, 
+It also offers an alternative to the liferay 'deploy' directory,
 called 'hotterdeploy' which can detect conflicting jar changes and will
 automatically undeploy and redeploy the affected portlet.
 (Change the liferay.auto.deploy.dir property in your settings.xml accordingly
@@ -51,6 +51,13 @@ class Console:
     def restore(cls):
         print("\033[u")
 
+def getElementsByTagName(xmldoc, name, parent_name='project'):
+    elements = xmldoc.getElementsByTagName(name)
+    els = []
+    for el in elements:
+        if parent_name and el.parentNode and el.parentNode.tagName == parent_name:
+            els.append(el)
+    return els
 
 class HotterDeployer(object):
     def __init__(self, workspace_directory, tomcat_directory):
@@ -61,50 +68,56 @@ class HotterDeployer(object):
         self.deploys = {}
         self.wm = pyinotify.WatchManager()
         self.notifier = pyinotify.Notifier(self.wm)
-        
+
         # Create our hotterdeploy directory and watch it for wars
         self.hotterdeploy_dir = os.path.abspath(os.path.join(tomcat_directory, '..', 'hotterdeploy'))
         if not os.path.exists(self.hotterdeploy_dir):
             os.mkdir(self.hotterdeploy_dir)
         self.wm.add_watch(self.hotterdeploy_dir, pyinotify.IN_CLOSE_WRITE, rec=False, auto_add=False, proc_fun=OnDeployHandler(hotterDeployer=self))
-        
+
         # Scan tomcat temp directory for deployed portlets
         self._scan_temp()
         self.wm.add_watch(os.path.join(tomcat_directory, 'temp'), pyinotify.IN_CREATE | pyinotify.IN_DELETE, rec=False, auto_add=False, proc_fun=OnTempDeployHandler(hotterDeployer=self))
-        
+
         # Scan tomcat webapps directory for deployed portlets
         self._scan_webapps()
         self.wm.add_watch(os.path.join(tomcat_directory, 'webapps'), pyinotify.IN_CREATE | pyinotify.IN_DELETE, rec=False, auto_add=False, proc_fun=OnWebappsDeployHandler(hotterDeployer=self))
-         
+
         # Scan the working directory for portlets
         self._scan_wd(workspace_directory)
-        
+
         def portlet_exclude_filter(path):
             return path.find('.svn') != -1 or not path.endswith('src/main/webapp/WEB-INF')
-        wdd = self.wm.add_watch(workspace_directory, pyinotify.IN_CREATE | pyinotify.IN_DELETE | pyinotify.IN_CLOSE_WRITE, 
+        wdd = self.wm.add_watch(workspace_directory, pyinotify.IN_CREATE | pyinotify.IN_DELETE | pyinotify.IN_CLOSE_WRITE,
                             rec=True, auto_add=True, proc_fun=WorkSpaceHandler(hotterDeployer=self),
-                            exclude_filter=portlet_exclude_filter)   
-                            
+                            exclude_filter=portlet_exclude_filter)
+
         def exclude_filter(path):
             return path.find('.svn') != -1 or path.find('src/main/webapp') == -1
-        self.wm.add_watch(workspace_directory, pyinotify.IN_CLOSE_WRITE, 
+        self.wm.add_watch(workspace_directory, pyinotify.IN_CLOSE_WRITE,
                             rec=True, auto_add=True, proc_fun=OnFileChangedHandler(hotterDeployer=self),
                             exclude_filter=exclude_filter)
-        
+
+        def class_exclude_filter(path):
+            return path.find('.svn') != -1 or path.find('target/classes') == -1
+        self.wm.add_watch(workspace_directory, pyinotify.IN_CLOSE_WRITE,
+                            rec=True, auto_add=True, proc_fun=OnClassChangedHandler(hotterDeployer=self),
+                            exclude_filter=class_exclude_filter)
+
 
         def handler(signum, frame):
             print signum, ' pressed, shutting down'
             self.stop()
-            
+
         signal.signal(signal.SIGTSTP, handler)
         signal.signal(signal.SIGINT, handler)
-        
+
         try:
             # Start LiveReload
             self.livereload_server = Server()
             t = Thread(target=self.livereload_server.serve)
             t.start()
-        
+
             self._print_status(True)
             self.notifier.loop()
         finally:
@@ -113,11 +126,11 @@ class HotterDeployer(object):
     def __del__(self):
         if os.path.exists(self.hotterdeploy_dir):
             os.rmdir(self.hotterdeploy_dir)
-            
+
     def stop(self):
         self.notifier.stop()
-        self.livereload_server.stop() 
-        
+        self.livereload_server.stop()
+
     def _print_status(self, initial=False):
         #if self.print_status_enabled:
         from livereload import LiveReloadHandler
@@ -147,7 +160,7 @@ class HotterDeployer(object):
         print '='*70
         if not initial:
             Console.restore()
-                
+
     def _scan_wd(self, directory):
        for file_name in os.listdir(directory):
            path = os.path.join(directory, file_name)
@@ -157,27 +170,27 @@ class HotterDeployer(object):
                # Deployed portlet name is derived from the war name
                if file_name == 'pom.xml':
                    xmldoc = minidom.parse(path)
-                   portlet_name = xmldoc.getElementsByTagName('artifactId')[0].firstChild.nodeValue
+                   portlet_name = getElementsByTagName(xmldoc, 'artifactId')[0].firstChild.nodeValue
                    if not any(map(lambda x: portlet_name.endswith('-'+x), ['portlet', 'hook', 'theme', 'web'])):
-                       portlet_name += '-'+xmldoc.getElementsByTagName('version')[0].firstChild.nodeValue
+                       portlet_name += '-'+getElementsByTagName(xmldoc, 'version')[0].firstChild.nodeValue
                    #TODO: fetch the war name if its specified in the build config
                    webapp_path = os.path.abspath(directory)
                    print portlet_name, webapp_path
                    self.portlets[webapp_path] = portlet_name
-    
+
     def _update_deploys(self):
         deploys = {}
         if hasattr(self, '_temp_deploys'):
             for name, path in self._temp_deploys.items():
                 deploys[name] = path
-        
+
         if hasattr(self, '_webapp_deploys'):
             for name, path in self._webapp_deploys.items():
                 if name not in deploys:
                     deploys[name] = path
-                
+
         self.deploys = deploys
-        
+
     def _scan_temp(self):
         path = os.path.join(self.tomcat_directory, 'temp')
         deploys = {}
@@ -190,14 +203,14 @@ class HotterDeployer(object):
                 if portlet_name not in deploys:
                     deploys[portlet_name] = []
                 deploys[portlet_name].append(deploy_path)
-        
+
         # Now only save the latest temp deploy dir per portlet
         for portlet_name, deploy_paths in deploys.items():
             deploys[portlet_name] = max(deploy_paths, key=os.path.getmtime)
-            
+
         self._temp_deploys = deploys
         self._update_deploys()
-    
+
     def _scan_webapps(self):
         path = os.path.join(self.tomcat_directory, 'webapps')
         deploys = {}
@@ -206,11 +219,11 @@ class HotterDeployer(object):
             deploy_path = os.path.join(path, deploy)
             if os.path.isdir(deploy_path):
                 deploys[deploy] = deploy_path
-            
+
         self._webapp_deploys = deploys
         self._update_deploys()
-                
-            
+
+
     def find_latest_temp_dir(self, portlet_name):
         '''
         Find the latest temp deploy directory for a given portlet:
@@ -218,11 +231,11 @@ class HotterDeployer(object):
         '''
         latest_subdir = self.deploys.get(portlet_name, None)
         return latest_subdir
-        
+
     def trigger_browser_reload(self):
         print 'reloading browser'
         self.livereload_server.reload()
-        
+
 
 class OnTempDeployHandler(pyinotify.ProcessEvent):
     def my_init(self, hotterDeployer):
@@ -239,11 +252,11 @@ class OnWebappsDeployHandler(pyinotify.ProcessEvent):
         self.hotterDeployer._scan_webapps()
         self.hotterDeployer._print_status()
 
-        
+
 class WorkSpaceHandler(pyinotify.ProcessEvent):
     def my_init(self, hotterDeployer):
         self.hotterDeployer = weakref.proxy(hotterDeployer)
-    def process_default(self, event):   
+    def process_default(self, event):
         if event.pathname.endswith('.xml'):
             print 'WorkSpaceHandler::process_default', event.pathname, event
             self.hotterDeployer._scan_wd(self.hotterDeployer.workspace_directory)
@@ -253,20 +266,41 @@ class WorkSpaceHandler(pyinotify.ProcessEvent):
 class OnFileChangedHandler(pyinotify.ProcessEvent):
     def my_init(self, hotterDeployer):
         self.hotterDeployer = weakref.proxy(hotterDeployer)
-        extension = '.jsp,.js,.css,.tag,.vm'
+        extension = '.jsp,.js,.css,.tag,.vm,.jspf'
         self.extensions = extension.split(',')
-       
+
+    def _is_jsp_hook(self, cwd, rel_path):
+        liferay_hook_path = os.path.join(cwd, 'src/main/webapp/WEB-INF/liferay-hook.xml')
+        if os.path.exists(liferay_hook_path):
+            xmldoc = minidom.parse(liferay_hook_path)
+            custom_jsp_dir = getElementsByTagName(xmldoc, 'custom-jsp-dir', 'hook')[0].firstChild.nodeValue
+            if rel_path.startswith(custom_jsp_dir[1:]+'/'):
+                rel_path = rel_path.split(custom_jsp_dir[1:]+'/')[1]
+                return rel_path
+
     def process_IN_CLOSE_WRITE(self, event):
         cwd = event.pathname.split('/src/main/webapp')[0]
+
         # Handle portlets
         portlet_name = self.hotterDeployer.portlets.get(cwd, None)
         if portlet_name:
             if all(not event.pathname.endswith(ext) for ext in self.extensions):
                 return
             rel_path = event.pathname.split(cwd+'/src/main/webapp')[1][1:]
-                
-            #Find latest dir
-            latest_subdir = self.hotterDeployer.find_latest_temp_dir(portlet_name)
+
+            jsp_hook = self._is_jsp_hook(cwd, rel_path)
+            if jsp_hook:
+                print 'JSP HOOK', rel_path
+                rel_path = jsp_hook
+                latest_subdir = os.path.join(self.hotterDeployer.tomcat_directory, 'webapps', 'ROOT')
+                dest_path = os.path.join(latest_subdir, rel_path)
+                print dest_path
+                if not os.path.exists(dest_path+'.hotterdeploy'):
+                    shutil.copy2(dest_path, dest_path+'.hotterdeploy')
+            else:
+                #Find latest dir
+                latest_subdir = self.hotterDeployer.find_latest_temp_dir(portlet_name)
+
             if not latest_subdir:
                 print '- Skipped {0} ({1} not deployed)'.format(rel_path, portlet_name)
             else:
@@ -274,19 +308,54 @@ class OnFileChangedHandler(pyinotify.ProcessEvent):
                 print '- Copying {0} ({1}) [{2}]'.format(rel_path, portlet_name, os.path.basename(latest_subdir))
                 if not os.path.exists(os.path.dirname(dest_path)):
                   os.makedirs(os.path.dirname(dest_path))
-                
-                shutil.copy2(event.pathname, dest_path)    
-                
+                print dest_path
+                shutil.copy2(event.pathname, dest_path)
+
                 self.hotterDeployer.trigger_browser_reload()
-               
+
+
+class OnClassChangedHandler(pyinotify.ProcessEvent):
+    def my_init(self, hotterDeployer):
+        self.hotterDeployer = weakref.proxy(hotterDeployer)
+
+    def process_IN_CLOSE_WRITE(self, event):
+        cwd = event.pathname.split('/target/classes')[0]
+
+        # Handle portlets
+        portlet_name = self.hotterDeployer.portlets.get(cwd, None)
+        print portlet_name
+        if portlet_name:
+            rel_path = event.pathname.split(cwd+'/target/classes')[1][1:]
+            print rel_path
+
+            latest_subdir = self.hotterDeployer.find_latest_temp_dir(portlet_name)
+
+            print rel_path
+
+            if not latest_subdir:
+                print '- Skipped {0} ({1} not deployed)'.format(rel_path, portlet_name)
+            else:
+                dest_path = os.path.join(latest_subdir, 'WEB-INF', 'classes', rel_path)
+                print '- Copying {0} ({1}) [{2}]'.format(rel_path, portlet_name, os.path.basename(latest_subdir))
+                if not os.path.exists(os.path.dirname(dest_path)):
+                  os.makedirs(os.path.dirname(dest_path))
+                print dest_path
+                shutil.copy2(event.pathname, dest_path)
+
+                #self.hotterDeployer.trigger_browser_reload()
+
+                from threading import Timer
+                t = Timer(1.2, self.hotterDeployer.trigger_browser_reload)
+                t.start()
+
 
 class OnDeployHandler(pyinotify.ProcessEvent):
     def my_init(self, hotterDeployer):
         self.hotterDeployer = weakref.proxy(hotterDeployer)
-        
-    def process_default(self, event):   
+
+    def process_default(self, event):
         d = Deploy(self.hotterDeployer, event.pathname, self.hotterDeployer.tomcat_directory)
-        d.start()  
+        d.start()
 
 
 class DeploymentTimedOutException(Exception):
@@ -302,20 +371,20 @@ class Deploy(Thread):
         self.war_path = war_path
         self.tomcat_directory = tomcat_directory
         super(Deploy, self).__init__()
-        
+
     def _get_portlet_name(self):
         with ZipFile(self.war_path, 'r') as war:
             with war.open('WEB-INF/portlet.xml') as xml:
                 xmldoc = minidom.parse(xml)
                 portlet_name = xmldoc.getElementsByTagName('portlet-name')[0].firstChild.nodeValue
                 return portlet_name
-    
+
     def _get_bundle_name(self):
         matcher = re.compile('(.+)-(\d\.\d\.\d(-SNAPSHOT)?)\.war')
         war_name = os.path.basename(self.war_path)
         match = matcher.match(war_name)
         return match.group(1)
-            
+
     def _undeploy(self, portlet_name):
         webapps_path = os.path.join(self.tomcat_directory, 'webapps', portlet_name)
         if os.path.exists(webapps_path):
@@ -323,12 +392,12 @@ class Deploy(Thread):
             return True
         else:
             return False
-    
+
     def _deploy(self):
         dst = os.path.abspath(os.path.join(self.tomcat_directory, '..', 'deploy/'))
         shutil.move(self.war_path, dst)
         return True
-          
+
     def _wait_for_string_in_log(self, string, action):
         matcher = re.compile(string)
         start = datetime.datetime.now()
@@ -347,17 +416,17 @@ class Deploy(Thread):
                         print '.',
                         if matcher.search(latest_data):
                             break
-                    
+
                     elapsed = datetime.datetime.now() - start
                     if elapsed > datetime.timedelta(minutes=1):
                         raise DeploymentTimedOutException()
-                    
+
     def do(self):
         bundle_name = self._get_bundle_name()
         latest_dir = self.hotterDeployer.find_latest_temp_dir(bundle_name)
-        
+
         try:
-            if latest_dir: 
+            if latest_dir:
                 # The portlet seems to be deployed, let's compare
                 needs_undeploy = check_for_lib_diffs(self.war_path, latest_dir)
                 if needs_undeploy:
@@ -365,18 +434,18 @@ class Deploy(Thread):
                     # We undeployed, now wait for LifeRay to notice
                     print 'Undeploying {0}'.format(bundle_name)
                     self._wait_for_string_in_log('for '+bundle_name+' (\w+) unregistered', lambda: self._undeploy(bundle_name))
-            
+
             print 'Deploying {0}'.format(bundle_name)
             self._wait_for_string_in_log('for '+bundle_name+' (\w+) available for use', lambda: self._deploy())
             self.hotterDeployer.trigger_browser_reload()
         except DeploymentTimedOutException as e:
-            print 'Deployment of {0} failed!!!'.format(bundle_name)  
-                                 
+            print 'Deployment of {0} failed!!!'.format(bundle_name)
+
     def run(self):
         self.do()
         self.hotterDeployer._print_status()
 
-  
+
 def check_for_lib_diffs(war_path, temp_portlet_path):
     '''
     Checks whether the jars in a war and on a deployed path are the same
@@ -400,7 +469,7 @@ def check_for_lib_diffs(war_path, temp_portlet_path):
                 p = Properties()
                 #p.load(prop) does not work
                 p._Properties__parse(prop.readlines())
-                
+
                 dep_jars.extend(p['portal-dependency-jars'].split(','))
         except KeyError:
             pass
@@ -434,7 +503,7 @@ def check_for_lib_diffs(war_path, temp_portlet_path):
         else:
             print '{0} is lingering'.format(filename)
             needs_undeploy = True
-            
+
     return needs_undeploy
 
 
@@ -444,7 +513,7 @@ def main():
     parser.add_argument('tomcat_directory', help='path to your tomcat')
 
     args = parser.parse_args()
-    
+
     deployer = HotterDeployer(args.workspace_directory, args.tomcat_directory)
     del deployer
 
